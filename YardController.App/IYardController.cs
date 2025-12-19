3c6f0261c299f060a241c4dd79bdfb048686ba14
Stefan Fjällemark <fjallemark@hotmail.com>
2025-12-19 09:13:10 +0100

    Fixing up some logging.

--- YardController.App/IYardController.cs
+++ YardController.App/IYardController.cs
@@ -1,5 +1,4 @@
 ﻿using Tellurian.Trains.Communications.Channels;
-using Tellurian.Trains.Interfaces.Accessories;
 using Tellurian.Trains.Protocols.LocoNet;
 
 namespace Tellurian.Trains.YardController;
@@ -14,8 +13,7 @@ public sealed class LoggingYardController(ILogger<LoggingYardController> logger)
     private readonly ILogger _logger = logger ?? throw new ArgumentNullException(nameof(logger));
     public Task SendSwitchCommandAsync(SwitchCommand command, CancellationToken cancellationToken)
     {
-
-        if (_logger.IsEnabled(LogLevel.Information)) _logger.LogInformation("Switch command received: {Command}", command);
+        if (_logger.IsEnabled(LogLevel.Information)) _logger.LogInformation("Switch command executed: {Command}", command);
         return Task.CompletedTask;
     }
 }
@@ -32,19 +30,21 @@ public sealed class TestYardController : IYardController
     public void Clear() => _commands.Clear();
 }
 
-public sealed class LocoNetYardController(ICommunicationsChannel communicationsChannel) : IYardController
+public sealed class LocoNetYardController(ICommunicationsChannel communicationsChannel, ILogger<LocoNetYardController> logger) : IYardController
 {
     private readonly ICommunicationsChannel _communicationsChannel = communicationsChannel ?? throw new ArgumentNullException(nameof(communicationsChannel));
+    private readonly ILogger<LocoNetYardController> _logger = logger;
+
     public async Task SendSwitchCommandAsync(SwitchCommand command, CancellationToken cancellationToken)
     {
         foreach (var turnoutCommand in command.ToTurnoutCommands())
         {
             if (cancellationToken.IsCancellationRequested) break;
-            await _communicationsChannel.SendAsync(turnoutCommand.GetBytesWithChecksum(), cancellationToken);
+            var data = turnoutCommand.GetBytesWithChecksum();
+            await _communicationsChannel.SendAsync(data, cancellationToken);
+            if (_logger.IsEnabled(LogLevel.Debug)) _logger.LogDebug("LocoNet turnout command sent: {Command}", Convert.ToHexString(data));
         }
+        if (_logger.IsEnabled(LogLevel.Information)) _logger.LogInformation("Switch command executed: {Command}", command);
     }
-
-    static Position GetPosition(SwitchDirection direction) =>
-        direction == SwitchDirection.Straight ? Position.ClosedOrGreen : Position.ThrownOrRed;
 }
 
