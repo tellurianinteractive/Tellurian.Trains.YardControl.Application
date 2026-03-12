a62c3bdfe4f060805423f55fedab74254b7f0af4
Stefan Fjällemark <fjallemark@hotmail.com>
2026-03-12 21:32:34 +0100

    Key read changes.

--- YardController.Model/Control/Extensions/KeyInfoExtensions.cs
+++ YardController.Model/Control/Extensions/KeyInfoExtensions.cs
@@ -63,29 +63,17 @@ public static class KeyInfoExtensions
 
 
         public char? ValidCharOrNull
-            => keyInfo.KeyChar == '=' ? '='
-            : keyInfo.Key switch
+            => keyInfo.KeyChar switch
             {
-                ConsoleKey.D0 or ConsoleKey.NumPad0 => '0',
-                ConsoleKey.D1 or ConsoleKey.NumPad1 => '1',
-                ConsoleKey.D2 or ConsoleKey.NumPad2 => '2',
-                ConsoleKey.D3 or ConsoleKey.NumPad3 => '3',
-                ConsoleKey.D4 or ConsoleKey.NumPad4 => '4',
-                ConsoleKey.D5 or ConsoleKey.NumPad5 => '5',
-                ConsoleKey.D6 or ConsoleKey.NumPad6 => '6',
-                ConsoleKey.D7 or ConsoleKey.NumPad7 => '7',
-                ConsoleKey.D8 or ConsoleKey.NumPad8 => '8',
-                ConsoleKey.D9 or ConsoleKey.NumPad9 => '9',
-                ConsoleKey.Multiply => '*',
-                ConsoleKey.Add => '+',
-                ConsoleKey.Subtract => '-',
-                ConsoleKey.Enter => '#',
-                ConsoleKey.Backspace => '<',
-                ConsoleKey.Decimal => '.',
-                ConsoleKey.Divide => '/',
-                ConsoleKey.Escape => '\x1b',
-                ConsoleKey.OemPlus => '=',
-                _ => null,
+                >= '0' and <= '9' => keyInfo.KeyChar,
+                '+' or '-' or '*' or '/' or '.' or '=' => keyInfo.KeyChar,
+                _ => keyInfo.Key switch
+                {
+                    ConsoleKey.Enter => '#',
+                    ConsoleKey.Backspace => '<',
+                    ConsoleKey.Escape => '\x1b',
+                    _ => null,
+                }
             };
     }
 
